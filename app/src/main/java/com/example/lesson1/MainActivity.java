229437410398230514f69f0642c3da73841878a9
Anatoly Saushkin <blond96@bk.ru>
2022-10-17 23:33:00 +0300

    Счетчик ворон

--- app/src/main/java/com/example/lesson1/MainActivity.java
+++ app/src/main/java/com/example/lesson1/MainActivity.java
@@ -1,46 +1,63 @@
 package com.example.lesson1;
 
+import static android.graphics.Typeface.DEFAULT;
+import static android.graphics.Typeface.MONOSPACE;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int mCount = 0;
+
+    private EditText mMessageEditText;
+    private CheckBox mColorCheckBox;
+    private Switch mBoldSwitch;
+    private ToggleButton mtoggleButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.first_app);
+        setTitle("Счетчик Ворон");
 
-        EditText firstnumber = findViewById(R.id.first_number);
-        EditText secondnumber = findViewById(R.id.second_number);
+        mMessageEditText = (EditText) findViewById(R.id.editText);
+        mColorCheckBox = (CheckBox) findViewById(R.id.checkBoxColor);
+        mBoldSwitch = (Switch) findViewById(R.id.switchBold);
+        mtoggleButton = (ToggleButton) findViewById(R.id.toggleButton);
 
-        Button compare = findViewById(R.id.compare);
+        Button mCrowsCounterButton = (Button) findViewById(R.id.buttonCounter);
+        mCrowsCounterButton.setOnClickListener(new View.OnClickListener() {
 
-        TextView result = findViewById(R.id.result);
 
-        compare.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                String txtFirstDigitValue = firstnumber.getText().toString();
-                String txtSecondDigitValue = secondnumber.getText().toString();
+            public void onClick(View v) {
+                if (mColorCheckBox.isChecked())
+                    mMessageEditText.setTextColor(Color.RED);
+                else
+                    mMessageEditText.setTextColor(Color.BLACK);
 
-                try {
+                if (mBoldSwitch.isChecked())
+                    mMessageEditText.setTypeface(Typeface.DEFAULT_BOLD);
+                else
+                    mMessageEditText.setTypeface(DEFAULT);
 
-                    Integer firstValue = Integer.valueOf(txtFirstDigitValue);
-                    Integer secondValue = Integer.valueOf(txtSecondDigitValue);
+                if (mtoggleButton.isChecked())
+                    mMessageEditText.setTypeface(MONOSPACE);
+                else
+                    mtoggleButton.setTypeface(DEFAULT);
 
-                    if (firstValue.equals(secondValue)) {
-                        result.setText("Equals!");
-                    } else {
-                        result.setText("Not Equals!");
-                    }
-                } catch (NumberFormatException e) {
-                    result.setText("Error!");
-                }
+                mMessageEditText.setText("Я насчитал " + ++mCount + " ворон");
             }
         });
     }
